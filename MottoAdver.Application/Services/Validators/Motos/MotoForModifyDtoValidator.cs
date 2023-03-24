ccaab0a692ff6aa590c3476c31c8296b342427e1
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-24 19:54:56 +0500

    Added "ModifyValidatorDto" to validator

---
+++ MottoAdver.Application/Services/Validators/Motos/MotoForModifyDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using MottoAdver.Application.DataTransferObjects;
+
+namespace MottoAdver.Application.Services.Validators.Motos;
+public class MotoForModifyDtoValidator : AbstractValidator<ModifyMotoDto>
+{
+    public MotoForModifyDtoValidator()
+    {
+        RuleFor(moto => moto)
+            .NotEmpty();
+
+        RuleFor(moto => moto.id)
+            .NotEqual(default(Guid));
+    }
+}
