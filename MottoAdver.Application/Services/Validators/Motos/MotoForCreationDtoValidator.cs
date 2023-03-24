f5ec58454dd7980b2c03ae3b16e4cf6355b88c74
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-24 19:54:36 +0500

    Added "creationvalidatorDto" to validator

---
+++ MottoAdver.Application/Services/Validators/Motos/MotoForCreationDtoValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using MottoAdver.Domain;
+
+namespace MottoAdver.Application.Services.Validators.Motos;
+public class MotoForCreationDtoValidator : AbstractValidator<CreationMotoDto>
+{
+    public MotoForCreationDtoValidator()
+    {
+        RuleFor(moto => moto)
+            .NotNull();
+
+        RuleFor(moto => moto.motoName)
+            .MaximumLength(30)
+            .NotEmpty();
+
+        RuleFor(moto => moto.charge)
+            .MaximumLength(50)
+            .NotEmpty();
+
+        RuleFor(moto => moto.distanceFullCharge)
+            .MaximumLength(50)
+            .NotEmpty();
+
+        RuleFor(moto => moto.maxWeight)
+            .NotEmpty();
+
+        RuleFor(moto => moto.year)
+            .NotEmpty();
+
+        RuleFor(moto => moto.maxSpeed)
+            .NotEmpty();
+
+        RuleFor(moto => moto.images)
+            .NotNull();
+    }
+}
