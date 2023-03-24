af8b1a8d0f8385112d69149b5ffd7953ce339212
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-24 19:53:13 +0500

    Added validation class to moto service

--- MottoAdver.Application/Services/Moto/MotoService.Validation.cs
+++ MottoAdver.Application/Services/Moto/MotoService.Validation.cs
@@ -1,5 +1,51 @@
-﻿namespace MottoAdver.Application.Services;
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MottoAdver.Application.DataTransferObjects;
+using MottoAdver.Application.Services.Validators.Motos;
+using MottoAdver.Domain;
+using System.Text.Json;
+
+namespace MottoAdver.Application.Services;
 public partial class MotoService
 {
+    public void ValidationMotoId(Guid motoId)
+    {
+        if (motoId == default)
+        {
+            throw new ValidationException($"The given userId is invalid: {motoId}");
+        }
+    }
+    public void ValidationStorageMoto(Motos moto, Guid motoId)
+    {
+        if (moto == null)
+        {
+            throw new ValidationException($"Moto object with id:{motoId} is null");
+        }
+    }
+    public void ValidationMotoForCreationDto(CreationMotoDto creationMotoDto)
+    {
+        var validationResult = new MotoForCreationDtoValidator().Validate(creationMotoDto);
 
+        ThrowValidationExceptionIfValidationIsInvalid(validationResult);
+    }
+    public void ValidationMotoForModifyDto(ModifyMotoDto modifyMotoDto)
+    {
+        var validationResult = new MotoForModifyDtoValidator().Validate(modifyMotoDto);
+
+        ThrowValidationExceptionIfValidationIsInvalid(validationResult);
+    }
+    private static void ThrowValidationExceptionIfValidationIsInvalid(ValidationResult validationResult)
+    {
+        if (validationResult.IsValid) return;
+
+        var errors = JsonSerializer
+            .Serialize(validationResult.Errors.Select(error => new
+            {
+                PropertyName = error.PropertyName,
+                ErrorMessage = error.ErrorMessage,
+                AttemptedValue = error.AttemptedValue
+            }));
+
+        throw new ValidationException(errors);
+    }
 }
