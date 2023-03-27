8b9ce9713058f614144893dbcdeb4f8d33d34a36
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-27 11:41:44 +0500

    addvertisement Validator added

---
+++ MottoAdver.Application/Services/Validators/Addvertisements/AddvertisementForCreationDtoValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using MottoAdver.Application.DataTransferObjects;
+using MottoAdver.Domain;
+
+namespace MottoAdver.Application.Services.Validators.Addvertisements;
+public class AddvertisementForCreationDtoValidator : AbstractValidator<CreationAddvertisementDto>
+{
+    public AddvertisementForCreationDtoValidator()
+    {
+        RuleFor(addvertisement => addvertisement)
+            .NotNull();
+
+        RuleFor(addvertisement => addvertisement.addvertiserFullName)
+            .MaximumLength(50)
+            .NotEmpty();
+
+        RuleFor(addvertisement => addvertisement.addvertiserTelegramUserName)
+            .NotEmpty();
+
+        RuleFor(addvertisement => addvertisement.addvertiserTellNumber)
+            .MaximumLength(15)
+            .NotEmpty();
+
+        RuleFor(addvertisement => addvertisement.price)
+            .NotEmpty();
+
+        RuleFor(addvertisement => addvertisement.motoId)
+            .NotEqual(default(Guid));
+
+        RuleFor(addvertisement => addvertisement.creationAddressDto)
+            .NotNull();
+    }
+}
