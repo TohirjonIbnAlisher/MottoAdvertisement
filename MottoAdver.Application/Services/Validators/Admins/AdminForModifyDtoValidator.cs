035f1e213d8f6ed8bd40cf5b08fdda5315d1eea3
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-27 11:41:50 +0500

    Admin Validator added

---
+++ MottoAdver.Application/Services/Validators/Admins/AdminForModifyDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using MottoAdver.Application.DataTransferObjects;
+
+namespace MottoAdver.Application.Services.Validators.Admins;
+public class AdminForModifyDtoValidator : AbstractValidator<ModifyAdminDto>
+{
+    public AdminForModifyDtoValidator()
+    {
+        RuleFor(admin => admin)
+            .NotNull();
+
+        RuleFor(admin => admin.id)
+            .NotEqual(default(Guid));
+    }
+}
