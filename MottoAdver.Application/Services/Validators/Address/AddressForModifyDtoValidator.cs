49224975c8521005ebe3e939ed3042b19fb6968e
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-27 11:41:23 +0500

    Address Validator added

---
+++ MottoAdver.Application/Services/Validators/Address/AddressForModifyDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using MottoAdver.Application.DataTransferObjects;
+
+namespace MottoAdver.Application.Services.Validators.Address;
+public class AddressForModifyDtoValidator : AbstractValidator<ModifyAddressDto>
+{
+    public AddressForModifyDtoValidator()
+    {
+        RuleFor(add => add)
+            .NotNull();
+
+        RuleFor(add => add.id)
+            .NotEqual(default(Guid));
+    }
+}
